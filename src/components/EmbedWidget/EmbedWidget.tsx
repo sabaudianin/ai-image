58d7df875284f85686247b2c53047ec950cbf3d3
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 <sabaudianin@gmail.com>
2025-10-15 16:37:16 +0200

    fixed widgetURL to public

--- src/components/EmbedWidget/EmbedWidget.tsx
+++ src/components/EmbedWidget/EmbedWidget.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 export const EmbedWidget = () => {
-  const widgetUrl =
-    process.env.NEXT_PUBLIC_WIDGET_URL ?? "https://localhost:5173";
+  // jakby zmienne środowiskowe nie zostalywstrzykniete
+  const FALLBACK_URL = "https://micro-frontend-ai-neon.vercel.app";
+
+  const widgetUrl = process.env.NEXT_PUBLIC_API_URL
+    ? `https://${process.env.NEXT_PUBLIC_API_URL}` // URL wstrzyknie Vercel
+    : FALLBACK_URL; // Ostateczny fallback do publicznego URL
+
   return (
     <section className="w-full min-h-screen">
       <iframe
