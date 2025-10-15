09355821aa5a2ef61dda557924251c58e01832ff
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 <sabaudianin@gmail.com>
2025-10-15 15:58:38 +0200

    feat screen height set

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { AuthWithGoogle } from "@/components/AuthWithGoogle/AuthWithGoogle";
 
 export default function LoginPage() {
   return (
-    <section className=" flex-1 grid place-items-center mx-auto space-y-8 w-full ">
+    <section className="min-h-[80dvh] flex-1 grid place-items-center mx-auto space-y-8 w-full ">
       <div className="flex flex-col gap-4 text-center bg-black/50 p-8 rounded-lg">
         <h2 className="text-2xl font-semibold p-4">To Start</h2>
         <p className="text-sm md:text-xl">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 ">
-      <Link href="/login">LOGIN PAGE</Link>
+    <section className="font-sans grid grid-rows-[20px_1fr] items-center justify-items-center min-h-[80dvh]  p-8 sm:p-20 ">
+      <p className="text-xl font-bold">MICRO FRONTEND AI APP</p>
+      <Link href="/login">LOGIN to continue(click here)</Link>
     </section>
   );
 }
